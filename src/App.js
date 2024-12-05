1e421b64537a439a5a2a5a8d328d0758ceb11ca2
VishnuK <vishnk@qa.com>
2024-12-05 16:09:24 +0000

    added my name to identify

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-      VAT CALCULATOR
+      VAT CALCULATOR from Vishnu
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
